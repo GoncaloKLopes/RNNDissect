3f051d02d4d3d21b395cc4d145e72e53b93402ff
goncalochincho <goncalochincho@gmail.com>
2019-05-05 11:58:58 +0100

    no more random outputs, dropout was to blame

--- rnndissect/main/models.py
+++ rnndissect/main/models.py
@@ -4,31 +4,40 @@ import torch.nn as nn
 class BidirectionalLSTM(nn.Module):
     def __init__(self, vocab_size, embedding_dim, hidden_dim, output_dim, n_layers, bidirectional, dropout):
         super().__init__()
-        
+
         self.embedding = nn.Embedding(vocab_size, embedding_dim)
-        self.rnn = nn.LSTM(embedding_dim, hidden_dim, num_layers=n_layers, bidirectional=bidirectional, dropout=dropout)
+        self.rnn = nn.LSTM(embedding_dim, hidden_dim, num_layers=n_layers, bidirectional=bidirectional)
         self.fc = nn.Linear(hidden_dim*2, output_dim)
         self.dropout = nn.Dropout(dropout)
-        
-    def forward(self, x):
-        
+
+    def forward(self, x, dropout=True):
+
         #x = [sent len, batch size]
-        
-        embedded = self.dropout(self.embedding(x))
-        
+        if dropout:
+            embedded = self.dropout(self.embedding(x))
+        else:
+            embedded = self.embedding(x)
+
         #embedded = [sent len, batch size, emb dim]
-        
-        output, (hidden, cell) = self.rnn(embedded)
-        
+
+        if dropout:
+            output, (hidden, cell) = self.dropout(self.rnn(embedded))
+        else:
+            output, (hidden, cell) = self.rnn(embedded)
+
         #output = [sent len, batch size, hid dim * num directions]
         #hidden = [num layers * num directions, batch size, hid dim]
         #cell = [num layers * num directions, batch size, hid dim]
-        
+
         #concat the final forward (hidden[-2,:,:]) and backward (hidden[-1,:,:]) hidden layers
         #and apply dropout
-        
-        hidden = self.dropout(torch.cat((hidden[-2,:,:], hidden[-1,:,:]), dim=1))
-                
+        if dropout:
+            hidden = self.dropout(torch.cat((hidden[-2,:,:], hidden[-1,:,:]), dim=1))
+        else:
+            hidden = torch.cat((hidden[-2,:,:], hidden[-1,:,:]), dim=1)
         #hidden = [batch size, hid dim * num directions]
-            
+
         return self.fc(hidden.squeeze(0))
+
+    def __call__(self, x):
+        return self.forward(x, dropout=False)
--- rnndissect/main/rnntoolbox.py
+++ rnndissect/main/rnntoolbox.py
@@ -17,11 +17,11 @@ class RNNToolbox:
 	def __init__(self, model_path, vocab_path):
 		"""
 		Args:
-			model (nn.Module) -> an instance of one of the model classes 
+			model (nn.Module) -> an instance of one of the model classes
 								 defined in models.py
 			vocab_path (string) -> path to the serialized vocabulary file.
 		"""
-		self.device = torch.device('cuda' if torch.cuda.is_available() 
+		self.device = torch.device('cuda' if torch.cuda.is_available()
 										  else 'cpu')
 
 		#load vocab first to get the number of inputs
@@ -32,26 +32,26 @@ class RNNToolbox:
 		INPUT_DIM = len(self.vocab)
 		EMBEDDING_DIM = 100
 		self.hidden_dim = 256
-		self.output_dim = 1	
+		self.output_dim = 1
 		self.n_layers = 2
 		BIDIRECTIONAL = True
 		DROPOUT = 0.5
 
-		self.model = BidirectionalLSTM(INPUT_DIM, EMBEDDING_DIM, self.hidden_dim, 
-								  self.output_dim, self.n_layers, BIDIRECTIONAL, 
+		self.model = BidirectionalLSTM(INPUT_DIM, EMBEDDING_DIM, self.hidden_dim,
+								  self.output_dim, self.n_layers, BIDIRECTIONAL,
 								  DROPOUT).to(self.device)
 
 		self.model.load_state_dict(torch.load(model_path))
 
 
-				
+
 	def classify(self, sentence):
 		"""
 		Performs the forward pass on a sentence, resulting in a score.
 		Args:
 			sentence (string) -> the sentence to classify.
 		"""
-		tensor = sentence_to_tensor(sentence, self.vocab)	
+		tensor = sentence_to_tensor(sentence, self.vocab)
 		prediction = torch.sigmoid(self.model(tensor))
 		return prediction.item()
 
@@ -66,7 +66,7 @@ class RNNToolbox:
 		gate_ids = set(zip(range(4), ["i", "f", "g", "o"]))
 		tensor_ids = ["i", "h"]
 		params = ["weight", "bias"]
-		pytorch_dict = self.model.state_dict()	
+		pytorch_dict = self.model.state_dict()
 		for rev in range(2):
 			revstr = ("_reverse" * rev)
 			for param in params:
@@ -90,11 +90,11 @@ class RNNToolbox:
 		"""
 		res = {}
 		tanh = torch.nn.Tanh()
-		
+
 		wstr = "weight_"
 		bstr = "bias_"
 		revstr = "_reverse"
-		
+
 		#how do I loop this
 		Wiil = wstr + f"ii{l}" + (revstr * rev)
 		biil = bstr + f"ii{l}" + (revstr * rev)
@@ -119,7 +119,7 @@ class RNNToolbox:
 		#tt is t+1 in variablenameland
 		cttl = f"c{t+1}{l}" + (revstr * rev)
 		httl = f"h{t+1}{l}" + (revstr * rev)
-		
+
 		res[itl] = self.infer_gate(params[Wiil], xt, params[biil],
 						params[Whil], ht, params[bhil], torch.sigmoid)
 
@@ -127,26 +127,26 @@ class RNNToolbox:
 						params[Whfl], ht, params[bhfl], torch.sigmoid)
 
 		res[gtl] = self.infer_gate(params[Wigl], xt, params[bigl],
-						params[Whgl], ht, params[bhgl], tanh)       
+						params[Whgl], ht, params[bhgl], tanh)
 
 		res[otl] = self.infer_gate(params[Wiol], xt, params[biol],
-						params[Whol], ht, params[bhol], torch.sigmoid) 
+						params[Whol], ht, params[bhol], torch.sigmoid)
 
 		res[cttl] = torch.add(torch.mul(res[ftl], ct), torch.mul(res[itl], res[gtl]))
 		res[httl] = torch.mul(res[otl], tanh(res[cttl]))
-			
+
 		return res
 
 	def infer_gate(self, i_w, x, b_i, h_w, h, b_h, fun):
 		input = torch.addmm(torch.unsqueeze(b_i, -1), i_w, x)
 		hidden = torch.addmm(torch.unsqueeze(b_h, -1), h_w, h)
-		
+
 		return fun(torch.add(input, hidden))
 
 	def forward_pass(self, embeddings, params):
 		result = {} #saves values of states and gates
 		revstr = "_reverse"
-			
+
 		for l in range(self.n_layers):
 			#random initialization of both hidden and cell states
 			result[f"h0{l}"] = torch.zeros(self.hidden_dim, 1).to(self.device)
@@ -154,7 +154,7 @@ class RNNToolbox:
 			#reverse aswell
 			result[f"h0{l}{revstr}"] = torch.zeros(self.hidden_dim, 1).to(self.device)
 			result[f"c0{l}{revstr}"] = torch.zeros(self.hidden_dim, 1).to(self.device)
-			
+
 			for t in range(len(embeddings)):
 				#if layer > 1, then the input isn't the model input, but the hidden state from the previous
 			#layer
@@ -166,10 +166,10 @@ class RNNToolbox:
 					input = torch.cat((result[f"h{t+1}{l-1}"], result[f"h{reverse_t+1}{l-1}{revstr}"]))
 					input_rev = torch.cat((result[f"h{reverse_t+1}{l-1}"],
 		  								  result[f"h{t+1}{l-1}{revstr}"]))
-						
-				result.update(self.infer_gates(input, result[f"h{t}{l}"], 
+
+				result.update(self.infer_gates(input, result[f"h{t}{l}"],
 											  result[f"c{t}{l}"], params, l, t))
-				result.update(self.infer_gates(input_rev, result[f"h{t}{l}{revstr}"], result[f"c{t}{l}{revstr}"], 
+				result.update(self.infer_gates(input_rev, result[f"h{t}{l}{revstr}"], result[f"c{t}{l}{revstr}"],
 											  params, l, t, rev=True))
 			return result
 
@@ -177,11 +177,11 @@ class RNNToolbox:
 		"""
 		Writes the activation values of the model corrresponding
 		to a specific input sentence into a .json file.
-		Args: 
+		Args:
 			sentence (string) -> input sentence
 		"""
 		json_path = "assets/data/"
-		encoded = sentence_to_tensor(sentence, self.vocab)	
+		encoded = sentence_to_tensor(sentence, self.vocab)
 		embeddings = self.model.dropout(self.model.embedding(encoded))
 		acts = self.forward_pass(embeddings, self.extract_params())
 		filename = "_".join([json_path, "acts", sentence.replace(" ", "_"), ".json"])
@@ -189,16 +189,16 @@ class RNNToolbox:
 		for (key,value) in acts.items():
  			acts_lists[key] = value.tolist()
 		with open(filename, "w+") as actsf:
-			json.dump(acts_lists, actsf) 
+			json.dump(acts_lists, actsf)
 		print("Activations saved to", filename)
-						
+
 if __name__ == "__main__":
 	input = sys.argv[1]
 
 	model_path = "assets/models/Bidirectional.pth"
 	vocab_path = "assets/data/vocab.pickle"
 	toolbox = RNNToolbox(model_path, vocab_path)
-	
+
 	toolbox.activations_to_json(input)
-	
+
 	print(f"{input} -> {toolbox.classify(input)}")
